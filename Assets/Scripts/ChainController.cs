2ba2f35b448f079baf5ea7b5571e705de803deca
TomTrenki <tomtrenki23@gmail.com>
2013-08-10 14:51:14 +0200

    ChainControlller, ChainLink
    Can extend and retract a chain, ChainLinks are automatically created and
    destroyed accordingly.

---
+++ Assets/Scripts/ChainController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainController : MonoBehaviour {
+	
+	public bool inputUp, inputDown;
+	public bool enableTestInput;
+	public ChainLink chainLinkPrefab;
+	public float chainLinkSize;
+	public float chainSpeed;
+	
+	ChainLink topChainLink;
+	float currentPos; // [0, chainLinkSize], controllable through inputs
+	
+	void Reset() {
+		chainLinkSize = 2.0f; // standard capsule height
+		chainSpeed = 5.0f;
+	}
+	
+	void Update() {
+		if (enableTestInput) {
+			inputUp = Input.GetKey(KeyCode.Q);
+			inputDown = Input.GetKey(KeyCode.A);
+		}
+	}
+	
+	void FixedUpdate() {
+		
+		if (topChainLink) {
+			var j = topChainLink.GetComponent<ConfigurableJoint>();
+			topChainLink.transform.position = transform.position - transform.up * currentPos;
+			topChainLink.transform.rotation = transform.rotation;
+			j.anchor = j.anchor;
+		}
+		
+		if (inputDown) {
+			currentPos += chainSpeed * Time.deltaTime;
+			if (currentPos > chainLinkSize) {
+				AddChainLink();
+				currentPos -= chainLinkSize;
+			}
+		}
+		
+		if (inputUp && topChainLink) {
+			currentPos -= chainSpeed * Time.deltaTime;
+			if (currentPos < 0.0f) {
+				RemoveChainLink();
+				currentPos += chainLinkSize;
+			}
+		}
+		
+	}
+	
+	void AddChainLink() {
+		var pos = transform.position;
+		var rot = transform.rotation;
+		if (topChainLink) {
+			pos = topChainLink.transform.position + topChainLink.transform.up * chainLinkSize;
+			rot = topChainLink.transform.rotation;
+		}
+		var cl = (ChainLink) Instantiate(chainLinkPrefab, pos, rot);
+		
+		cl.next = topChainLink;
+		if (topChainLink) {
+			var j = topChainLink.GetComponent<ConfigurableJoint>();
+			j.connectedBody = cl.rigidbody;
+		}
+		
+		topChainLink = cl;
+	}
+	
+	void RemoveChainLink() {
+		if (!topChainLink) return;
+		
+		if (topChainLink.next) {
+			var j = topChainLink.next.GetComponent<ConfigurableJoint>();
+			j.connectedBody = null;
+		}
+		
+		Destroy(topChainLink.gameObject);
+		topChainLink = topChainLink.next;
+	}
+	
+}
---
+++ Assets/Scripts/ChainLink.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+public class ChainLink : MonoBehaviour {
+	
+	public ChainLink next;
+	
+}
